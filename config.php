5a9f6880a6be18ae94a7b807578506823defd77c
Qohh <d1041231063@student.untan.ac.id>
2026-06-12 06:02:36 +0700

    ubah config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $koneksi = mysqli_connect(
     "jo80kso4w8gswcsg48osc8k0",
     "mysql",
     "3SAW6x4yndARgn9WABLjx8bvo8u9tR4hLSHuHqosP4atyaCdBH8vCRtmqCC6Fg8J",
-    "gis-kemiskinan",
+    "default",
     3306
 );
 
